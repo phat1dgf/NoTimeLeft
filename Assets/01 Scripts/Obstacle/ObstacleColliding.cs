ef37c11d4aa591b45c3c437dacd873f6a72e65d1
QiJi- <phamphat2262@gmail.com>
2025-04-04 09:06:26 +0700

    obstacle update

---
+++ Assets/01 Scripts/Obstacle/ObstacleColliding.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleColliding : M_MonoBehaviour
+{
+    [SerializeField] private Collider2D _colli;
+    protected override void Reset()
+    {
+        base.Reset();
+        this._colli.isTrigger = true;
+    }
+    protected override void LoadComponents()
+    {
+        base.LoadComponents();
+        LoadCollider();
+    }
+    private void LoadCollider()
+    {
+        if (this._colli != null) return;
+        this._colli = this.GetComponent<Collider2D>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag(CONSTANT.Tag_Player))
+        {
+            IGetHit vehicle = collision.GetComponent<IGetHit>();
+            if (vehicle != null)
+            {
+                vehicle.GetHit();
+            }
+        }
+    }
+}
---
+++ Assets/01 Scripts/Obstacle/ObstacleSpawner.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawner : M_MonoBehaviour
+{
+    [SerializeField] private GameObject obstacle;
+
+
+}
